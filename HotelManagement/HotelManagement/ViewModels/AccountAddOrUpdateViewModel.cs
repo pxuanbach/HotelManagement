fcefaa4c9551a33b89b786c6fdd0aff749dfda75
pxuanbach <pxuanbach1412@gmail.com>
2021-10-08 13:54:49 +0700

    update UI Account view

---
+++ HotelManagement/HotelManagement/ViewModels/AccountAddOrUpdateViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.ViewModels
+{
+    class AccountAddOrUpdateViewModel : BaseViewModel
+    {
+        
+        public AccountAddOrUpdateViewModel()
+        {
+
+        }
+
+        
+    }
+}
--- HotelManagement/HotelManagement/ViewModels/AccountViewModel.cs
+++ HotelManagement/HotelManagement/ViewModels/AccountViewModel.cs
@@ -6,11 +6,100 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows;
+using System.Collections.ObjectModel;
+using HotelManagement.Models;
 
 namespace HotelManagement.ViewModels
 {
     class AccountViewModel : BaseViewModel
     {
-        
+        #region Properties
+        private string _username;
+        public string Username { get { return _username; } set { _username = value; OnPropertyChanged(); } }
+
+        private List<string> _roles;
+        public List<string> Roles { get { return _roles; } set { _roles = value; OnPropertyChanged(); } }
+
+        private string _selectedRole;
+        public string SelectedRole { get { return _selectedRole; } set { _selectedRole = value; OnPropertyChanged(); } }
+
+        private bool _isOpenDialog;
+        public bool IsOpenDialog { get { return _isOpenDialog; } set { _isOpenDialog = value; OnPropertyChanged(); } }
+
+        private ObservableCollection<ACCOUNT> _accounts;
+        public ObservableCollection<ACCOUNT> Accounts
+        {
+            get { return _accounts; }
+            set { _accounts = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        #region Command
+        public ICommand AddNewAccountCommand { get; set; }
+        public ICommand SaveNewAccountCommand { get; set; }
+        public ICommand SearchAccountCommand { get; set; }
+        public ICommand EditAccountCommand { get; set; }
+        #endregion
+
+        public AccountViewModel()
+        {
+            InitProperties();
+
+            AddNewAccountCommand = new RelayCommand<object>((p) =>
+            {
+                return true;
+            }, (p) =>
+            {
+                IsOpenDialog = true;
+            });
+
+            SaveNewAccountCommand = new RelayCommand<object>((p) =>
+            {
+                if (string.IsNullOrEmpty(Username))
+                    return false;
+                return true;
+            }, (p) =>
+            {
+                Save(p);
+            });
+
+            EditAccountCommand = new RelayCommand<ACCOUNT>((p) =>
+            {
+                return true;
+            }, (p) =>
+            {
+                IsOpenDialog = true;
+                Username = p.username;
+                SelectedRole = p.permission;
+            });
+        }
+
+        void InitProperties()
+        {
+            Accounts = new ObservableCollection<ACCOUNT>();
+            var objList = DataProvider.Instance.DB.ACCOUNTs.Where(p => p.permission != "Admin").ToList();
+            foreach (var obj in objList)
+            {
+                ACCOUNT acc = new ACCOUNT();
+                acc = obj;
+                Accounts.Add(acc);
+            }    
+
+            IsOpenDialog = false;
+
+            Roles = new List<string>();
+            Roles.Add("Reservation");
+            Roles.Add("Receptionist");
+            Roles.Add("Cashier");
+            Roles.Add("Undefined");
+
+            SelectedRole = "Undefined";
+        }
+
+        void Save(object p)
+        {
+            IsOpenDialog = false;
+        }
     }
 }
