9a0a645d049584ca3a4a5024069a1aa384571ba2
pxuanbach <pxuanbach1412@gmail.com>
2021-10-23 18:16:04 +0700

    Update Invoice
    - Add dialoghost Fees
    - Validate input data
    - Fix load fees logic in invoice details

--- HotelManagement/HotelManagement/ViewModels/Invoice/InvoiceViewModel.cs
+++ HotelManagement/HotelManagement/ViewModels/Invoice/InvoiceViewModel.cs
@@ -117,10 +117,45 @@ namespace HotelManagement.ViewModels
         private double _surcharge;
         public double Surcharge { get { return _surcharge; } set { _surcharge = value; OnPropertyChanged(); } }
 
+        public long TotalMoneyNumber { get; set; }
+
         private string _totalMoney;
         public string TotalMoney { get { return _totalMoney; } set { _totalMoney = value; OnPropertyChanged(); } }
         #endregion
 
+        #endregion
+
+        #region Dialog Properties
+        private bool _isOpenDialog;
+        public bool IsOpenDialog
+        {
+            get { return _isOpenDialog; }
+            set { _isOpenDialog = value; OnPropertyChanged(); }
+        }
+
+        private string _errorMessageDialog;
+        public string ErrorMessageDialog 
+        { 
+            get { return _errorMessageDialog; } 
+            set { _errorMessageDialog = value; OnPropertyChanged(); } 
+        }
+
+        private double _surchargeDialog;
+        public double SurchargeDialog { get { return _surchargeDialog; } set { _surchargeDialog = value; OnPropertyChanged(); } }
+
+        private double _earlyCheckinFeeDialog;
+        public double EarlyCheckinFeeDialog 
+        { 
+            get { return _earlyCheckinFeeDialog; } 
+            set { _earlyCheckinFeeDialog = value; OnPropertyChanged(); } 
+        }
+
+        private double _lateCheckoutFeeDialog;
+        public double LateCheckoutFeeDialog
+        {
+            get { return _lateCheckoutFeeDialog; }
+            set { _lateCheckoutFeeDialog = value; OnPropertyChanged(); }
+        }
         #endregion
 
         public string StatusSelected { get; set; }
@@ -140,6 +175,8 @@ namespace HotelManagement.ViewModels
         public ICommand ListviewSelectionChangedCommand { get; set; }
         public ICommand ClearDetailCommand { get; set; }
         public ICommand FolioOfRoomCommand { get; set; }
+        public ICommand ShowFeesCommand { get; set; }
+        public ICommand SaveFeesCommand { get; set; }
         public ICommand CheckOutCommand { get; set; }
         public ICommand ExportCommand { get; set; }
         #endregion
@@ -147,6 +184,7 @@ namespace HotelManagement.ViewModels
         public InvoiceViewModel()
         {
             InitProperties();
+            InitDialogProperties();
 
             SearchReservationCommand = new RelayCommand<object>((p) =>
             {
@@ -205,6 +243,29 @@ namespace HotelManagement.ViewModels
                 OpenFolioOfRoomWindow(p);
             });
 
+            ShowFeesCommand = new RelayCommand<object>((p) =>
+            {
+                return true;
+            }, (p) =>
+            {
+                IsOpenDialog = true;
+                InitDialogProperties();
+            });
+
+            SaveFeesCommand = new RelayCommand<object>((p) =>
+            {
+                return true;
+            }, (p) =>
+            {
+                var charge = DataProvider.Instance.DB.CHARGES.First();
+                charge.surcharge = SurchargeDialog;
+                charge.early_checkin_fee = EarlyCheckinFeeDialog;
+                charge.late_checkout_fee = LateCheckoutFeeDialog;
+                DataProvider.Instance.DB.SaveChanges();
+
+                IsOpenDialog = false;
+            });
+
             CheckOutCommand = new RelayCommand<ListView>((p) =>
             {
                 if (StatusSelected != "Operational")
@@ -215,11 +276,13 @@ namespace HotelManagement.ViewModels
                 {
                     if (p.SelectedIndex == -1)
                         return false;
-                }    
+                }
                 return true;
             }, (p) =>
             {
-
+                ClearDetailProperties();
+                CheckOut((RESERVATION)p.SelectedItem);
+                LoadReservations();
             });
 
             ExportCommand = new RelayCommand<ListView>((p) =>
@@ -238,7 +301,6 @@ namespace HotelManagement.ViewModels
 
         void InitProperties()
         {
-            
             Guests = new ObservableCollection<GUEST>();
             Rooms = new ObservableCollection<RoomDisplayItem>();
             Folio = new ObservableCollection<FolioDisplayItem>();
@@ -251,8 +313,18 @@ namespace HotelManagement.ViewModels
             SelectedSearchType = "ID";
         }
 
+        void InitDialogProperties()
+        {
+            var charge = DataProvider.Instance.DB.CHARGES.First();
+            ErrorMessageDialog = "";
+            SurchargeDialog = charge.surcharge.Value;
+            EarlyCheckinFeeDialog = charge.early_checkin_fee.Value;
+            LateCheckoutFeeDialog = charge.late_checkout_fee.Value;
+        }
+
         void ClearDetailProperties()
         {
+            ReservationIdSelected = 0;
             Arrival = "";
             Departure = "";
 
@@ -283,6 +355,7 @@ namespace HotelManagement.ViewModels
             EarlyCheckinFee = 0;
             LateCheckoutFee = 0;
             Surcharge = 0;
+            TotalMoneyNumber = 0;
             TotalMoney = "";
         }
 
@@ -291,6 +364,18 @@ namespace HotelManagement.ViewModels
         {
             Reservations = new ObservableCollection<RESERVATION>(
                 DataProvider.Instance.DB.RESERVATIONs.Where(x => x.status == StatusSelected));
+
+            if (StatusSelected == "Operational")
+            {
+                foreach (var reservation in Reservations)
+                {
+                    if ((reservation.departure.Value - DateTime.Now).TotalDays < 0)
+                    {
+                        reservation.late_checkout = true;
+                        DataProvider.Instance.DB.SaveChanges();
+                    }    
+                }    
+            }    
         }
 
         void LoadItemSelected(RESERVATION p)
@@ -362,18 +447,35 @@ namespace HotelManagement.ViewModels
             RoomsTotalMoney = SeparateThousands(roomsTotalMoney.ToString());
             FolioTotalMoney = SeparateThousands(folioTotalMoney.ToString());
 
-            if (p.early_checkin.Value)
-            {
-                EarlyCheckinFee = DataProvider.Instance.DB.CHARGES.First().early_checkin_fee.Value;
-            }   
-            if (p.late_checkout.Value)
-            {
-                LateCheckoutFee = DataProvider.Instance.DB.CHARGES.First().late_checkout_fee.Value;
-            }
-            Surcharge = DataProvider.Instance.DB.CHARGES.First().surcharge.Value;
+            LoadFeeByStatus(p);
 
-            double totalMoney = CalculateTotalMoney(sumRoomPrice, roomsTotalMoney, folioTotalMoney);
-            TotalMoney = SeparateThousands(totalMoney.ToString());
+            TotalMoneyNumber = CalculateTotalMoney(sumRoomPrice, roomsTotalMoney, folioTotalMoney);
+            TotalMoney = SeparateThousands(TotalMoneyNumber.ToString());
+        }
+
+        void LoadFeeByStatus(RESERVATION p)
+        {
+            if (StatusSelected == "Operational")
+            {
+                if (p.early_checkin.Value)
+                {
+                    EarlyCheckinFee = DataProvider.Instance.DB.CHARGES.First().early_checkin_fee.Value;
+                }
+                if (p.late_checkout.Value)
+                {
+                    LateCheckoutFee = DataProvider.Instance.DB.CHARGES.First().late_checkout_fee.Value;
+                }  
+                
+                Surcharge = DataProvider.Instance.DB.CHARGES.First().surcharge.Value;
+            }
+            if (StatusSelected == "Completed")
+            {
+                var invoice = DataProvider.Instance.DB.INVOICEs.SingleOrDefault(x => x.reservation_id == p.id);
+
+                EarlyCheckinFee = invoice.early_checkin_fee.Value;
+                LateCheckoutFee = invoice.late_checkout_fee.Value;
+                Surcharge = invoice.surcharge.Value;
+            }
         }
         #endregion
         
@@ -405,11 +507,11 @@ namespace HotelManagement.ViewModels
             return sumRoomPrice * (int)(departure - arrival).TotalDays;
         }
 
-        double CalculateTotalMoney(long sumRoomPrice, long roomsTotalMoney, long folioTotalMoney)
+        long CalculateTotalMoney(long sumRoomPrice, long roomsTotalMoney, long folioTotalMoney)
         {
             double fee = sumRoomPrice * (EarlyCheckinFee + LateCheckoutFee)/100;
             double totalMoney = (roomsTotalMoney + folioTotalMoney) * (100 + Surcharge)/100;
-            return totalMoney + fee;
+            return (long)(totalMoney + fee);
         }
 
         void Search()
@@ -440,6 +542,21 @@ namespace HotelManagement.ViewModels
             vm.InitProperties();
             wd.DataContext = vm;
             wd.ShowDialog();
+        }
+
+        void CheckOut(RESERVATION p)
+        {
+            p.status = "Completed";
+            INVOICE invoice = new INVOICE()
+            {
+                reservation_id = p.id,
+                total_money = Convert.ToDecimal(TotalMoneyNumber),
+                surcharge = Surcharge,
+                early_checkin_fee = EarlyCheckinFee,
+                late_checkout_fee = LateCheckoutFee,
+            };
+            DataProvider.Instance.DB.INVOICEs.Add(invoice);
+            DataProvider.Instance.DB.SaveChanges();
         }
     }
 }
