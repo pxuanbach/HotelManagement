13e497ea0bb2dc136e6fbfa5e4f5529b6e3e9cc3
pxuanbach <pxuanbach1412@gmail.com>
2021-11-15 18:51:21 +0700

    Report Top service used

---
+++ HotelManagement/HotelManagement/Models/ServiceReportItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Models
+{
+    class ServiceReportItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public int Amount { get; set; }
+
+        public ServiceReportItem(int id, string name, string price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public ServiceReportItem(int id, string name, string price, int amount)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Amount = amount;
+        }
+    }
+}
